9562fa7f5c2da7e4ffc0b460b428227d940f595f
ZekielAsh <sherlock-ezequiel@hotmail.com>
2022-06-12 21:22:39 -0300

    Implementacion de ZonaDeCobertura y ajuste en Ubicacion

---
+++ src/zonaCobertura/ZonaDeCobertura.java
@@ -0,0 +1,39 @@
+package zonaCobertura;
+
+import ubicacion.Ubicacion;
+
+public class ZonaDeCobertura {
+	private String nombre;
+	private Ubicacion epicentro;
+	private float radio;
+	
+	public ZonaDeCobertura(String nombre, float latitud, float longitud, float radio) {
+		this.setNombre(nombre);
+		this.setEpicentro(latitud, longitud);
+		this.setRadio(radio);
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Ubicacion getEpicentro() {
+		return epicentro;
+	}
+	public void setEpicentro(float latitud, float longitud) {
+		this.epicentro = new Ubicacion(latitud, longitud);
+	}
+		public float getRadio() {
+		return radio;
+	}
+	public void setRadio(float radio) {
+		this.radio = radio;
+	}
+	
+	public boolean seSolapan(ZonaDeCobertura zona) {
+		return (this.radio + zona.radio >= 
+				this.getEpicentro().distancia(zona.getEpicentro()));
+	}
+	
+}
