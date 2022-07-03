f3d287d6e4581532b0e1117686211e6310b6237a
ZekielAsh <sherlock-ezequiel@hotmail.com>
2022-07-03 19:37:28 -0300

    Implementacion de Organizacion antes de aplicar patron Observer

--- CazadorDeVinchucas/organizacion/Organizacion.java
+++ CazadorDeVinchucas/organizacion/Organizacion.java
@@ -1,15 +1,24 @@
 package organizacion;
 
+import java.util.List;
+
 import ubicacion.Ubicacion;
+import zonaCobertura.ZonaDeCobertura;
 
 public class Organizacion {
 	private TipoDeOrganizacion tipo;
 	private Ubicacion ubicacion;
 	private int trabajadores;
+	private List<ZonaDeCobertura> zonasDeCobertura;
 	
-	public Organizacion(Ubicacion locacion, int cantidadDeTrabajadores) {
+	public Organizacion(Ubicacion locacion, int cantidadDeTrabajadores, List<ZonaDeCobertura> zonas) {
 		this.setUbicacion(locacion);
 		this.setTrabajadores(cantidadDeTrabajadores);
+		this.setZonasDeCobertura(zonas);
+	}
+	
+	public TipoDeOrganizacion getTipoDeOrganizacion() {
+		return tipo;
 	}
 
 	public Ubicacion getUbicacion() {
@@ -19,10 +28,6 @@ public class Organizacion {
 	public void setUbicacion(Ubicacion locacion) {
 		this.ubicacion = locacion;
 	}
-	
-	public TipoDeOrganizacion getTipoDeOrganizacion() {
-		return tipo;
-	}
 
 	public int getCantidadDeTrabajadores() {
 		return trabajadores;
@@ -31,6 +36,21 @@ public class Organizacion {
 	public void setTrabajadores(int cantidadDeTrabajadores) {
 		this.trabajadores = cantidadDeTrabajadores;
 	}
+
+	public List<ZonaDeCobertura> getZonasDeCobertura() {
+		return zonasDeCobertura;
+	}
+
+	public void setZonasDeCobertura(List<ZonaDeCobertura> zonas) {
+		this.zonasDeCobertura = zonas;
+	}
 	
+	public void agregarZona(ZonaDeCobertura zona) {
+		this.zonasDeCobertura.add(zona);
+	}
+	
+	public void sacarZona(ZonaDeCobertura zona) {
+		this.zonasDeCobertura.remove(zona);
+	}
 	
 }
